f18dd8132dd35645dff10ee58d82f4876de9a5c8
Lev <lzelenin@njit.edu>
2025-03-22 23:20:20 -0400

    Implemented multiplication, fixed typos
    Signed-off-by: Lev <lzelenin@njit.edu>

--- calculator/commands/multiply/multiply.py
+++ calculator/commands/multiply/multiply.py
@@ -1,4 +1,4 @@
-"""Plugin for the subtraction command - subtracts the arguments together and returns the result.
+"""Plugin for the multiplication command - multiplies the arguments together and returns the result.
 
 Uses decimal data type.
 """
@@ -9,34 +9,34 @@ from decimal import Decimal, InvalidOperation
 from calculator.command import Command
 from calculator.command_input import CommandInput
 from calculator.command_output import CommandOutput
-from calculator.commands.subtract.exceptions import (
-    InvalidSubtractionArguments,
-    MissingSubtractionArguments
+from calculator.commands.multiply.exceptions import (
+    InvalidMultiplicationArguments,
+    MissingMultiplicationArguments
 )
 
 
-class Subtract(Command):
-    """Subtract the arguments from each other"""
+class Multiply(Command):
+    """Multiply the arguments together"""
     # command string regex this plugin will be responsible for
     # ignore leading whitespace, make it case insensitive
-    COMMAND_PATTERN = re.compile(r"^\s*(subtract|minus|\-|subtraction|sub|s)$", re.IGNORECASE)
+    COMMAND_PATTERN = re.compile(r"^\s*(multiply|times|\*|multiplication|mult|m)$", re.IGNORECASE)
 
     def __init__(self, cmd: CommandInput) -> None:
         self.cmd = cmd
-        logging.debug("Subtract plugin object initialized")
+        logging.debug("Multiply plugin object initialized")
 
 
     @classmethod
     def in_scope(cls, cmd: CommandInput) -> bool:
         """Return T/F if the command is in this plugin's scope"""
-        logging.debug(f"Subtract plugin scope check for {cmd.command}")
+        logging.debug(f"Multiply plugin scope check for {cmd.command}")
         return bool(cls.COMMAND_PATTERN.match(cmd.command))
 
 
     def validate(self) -> None:
         """Verify arguments are valid decimals - LBYL"""
         if len(self.cmd.args.keys()) < 2:
-            raise MissingSubtractionArguments
+            raise MissingMultiplicationArguments
 
         bad_args = []
         for arg_value in self.cmd.args.values():
@@ -46,16 +46,16 @@ class Subtract(Command):
                 bad_args.append(arg_value)
 
         if len(bad_args) > 0:
-            raise InvalidSubtractionArguments(bad_args)
+            raise InvalidMultiplicationArguments(bad_args)
 
 
     def execute(self) -> CommandOutput:
-        """Subtract arguments together, return CommandOutput with difference"""
-        logging.debug(f"Subtracting {self.cmd.args.values()}")
+        """Multiply arguments together, return CommandOutput with product"""
+        logging.debug(f"Multiplying {self.cmd.args.values()}")
 
-        out_diff = Decimal(self.cmd.args["argument_1"])
-        for i in range(2, self.cmd.num_args + 1):
-            out_diff -= Decimal(self.cmd.args[f"argument_{i}"])
+        out_product = Decimal(1)
+        for i in range(1, self.cmd.num_args + 1):
+            out_product *= Decimal(self.cmd.args[f"argument_{i}"])
 
-        logging.debug(f"Returning difference {out_diff}")
-        return CommandOutput(str(out_diff))
+        logging.debug(f"Returning difference {out_product}")
+        return CommandOutput(str(out_product))
