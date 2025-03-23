feebef98e3baa9a162100d18ed17f02997aca11e
Lev <lzelenin@njit.edu>
2025-03-22 20:56:36 -0400

    Refactored main
    Signed-off-by: Lev <lzelenin@njit.edu>

--- calculator/main.py
+++ calculator/main.py
@@ -1,33 +1,14 @@
 """Module containing main function and application set up"""
 
-import os
 import logging
-import logging.config
-from pathlib import Path
-from dotenv import load_dotenv
+from calculator.setup_env import setup_env
 from calculator.cli import CLI
 
 
 def main():
     """Set up environment and run the calculator"""
-    # set up paths and file names
-    base_path = Path(__file__).resolve().parent
-    load_dotenv(base_path.parent / ".env")
-    log_name = os.getenv("LOG_NAME")
-    log_dir_name = os.getenv("LOG_DIR_NAME")
-    log_config_name = os.getenv("LOG_CONFIG_NAME")
-    log_config = base_path / log_config_name
-    logs_path = base_path.parent / log_dir_name
-
-    # set up logging
-    os.makedirs(logs_path, exist_ok = True)
-    logging.config.fileConfig(
-        str(log_config),
-        defaults={"logs_path": str(logs_path / log_name)}
-    )
-    logging.info("Initialized environment")
-
-    # run the calculator
+    logging.info("Setting up env")
+    setup_env()
     c = CLI()
     logging.info("Starting CLI")
     c.start()
---
+++ calculator/setup_env.py
@@ -0,0 +1,26 @@
+"""Module defining environment setup"""
+
+import os
+import logging.config
+from pathlib import Path
+from dotenv import load_dotenv
+
+
+def setup_env() -> None:
+    """Set up environment"""
+    # set up paths and file names
+    base_path = Path(__file__).resolve().parent
+    load_dotenv(base_path.parent / ".env")
+    log_name = os.getenv("LOG_NAME")
+    log_dir_name = os.getenv("LOG_DIR_NAME")
+    log_config_name = os.getenv("LOG_CONFIG_NAME")
+    log_config = base_path / log_config_name
+    logs_path = base_path.parent / log_dir_name
+
+    # set up logging
+    os.makedirs(logs_path, exist_ok = True)
+    logging.config.fileConfig(
+        str(log_config),
+        defaults={"logs_path": str(logs_path / log_name)}
+    )
+    logging.debug("Initialized environment")
